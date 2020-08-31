a0d170aef748758e0d2738d0a945bcd21519dcca
Rashad Khoshnow <rashad.khoshnow@gmail.com>
2020-08-31 13:07:59 +0200

    Aufgabe erledigt

---
+++ aufgabe/zweiPunktDrei.java
@@ -0,0 +1,12 @@
+public class zweiPunktDrei {
+    public static void main(String[] args) {
+        double time = 12.30;
+
+        if (time >= 8 && time < 12 || time > 13 && time <= 16) {
+            System.out.println("work time");
+            }
+        else if (time >=12 && time <=13){
+            System.out.println("break");
+        }
+    }
+}
